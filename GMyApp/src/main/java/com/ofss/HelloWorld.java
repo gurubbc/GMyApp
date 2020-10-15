202a5259c6beaf4cfbfde8a8aba4708fd0fef752
Guru <gurumurthy.ramamurthy@gmail.com>
2020-10-15 16:51:01 +0530

    jenkins build - trial 1

--- GMyApp/src/main/java/com/ofss/HelloWorld.java
+++ GMyApp/src/main/java/com/ofss/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("This is my first program in git");
-		Customer c=new Customer("Albert","New York");
+		Customer c=new Customer("Aniraj","Indore");
 		System.out.println(c);
 	}
 
