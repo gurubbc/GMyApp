fdfcc641dac57372b8fe12e9029458eea168207e
Guru <gurumurthy.ramamurthy@gmail.com>
2020-10-15 17:04:52 +0530

    Jenkins - continuous delivery to client's folder (2nd try)

--- GMyApp/src/main/java/com/ofss/HelloWorld.java
+++ GMyApp/src/main/java/com/ofss/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("This is my first program in git");
-		Customer c=new Customer("Guru","Bangalore");
+		Customer c=new Customer("GuruMurthy","Bangalore");
 		System.out.println(c);
 	}
 
