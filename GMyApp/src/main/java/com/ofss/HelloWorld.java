aa80d285c462aad34bf2c33282f34b41fd2d7fdb
Guru <gurumurthy.ramamurthy@gmail.com>
2020-10-15 16:53:52 +0530

    Maven Build - automatic - 2nd try

--- GMyApp/src/main/java/com/ofss/HelloWorld.java
+++ GMyApp/src/main/java/com/ofss/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("This is my first program in git");
-		Customer c=new Customer("Aniraj","Indore");
+		Customer c=new Customer("Anjitha","Calicut");
 		System.out.println(c);
 	}
 
